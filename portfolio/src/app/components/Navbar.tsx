647e17de399263c85000e6efa12bb81163a61c3f
Justin Hill <jhill@figma.com>
2025-01-16 08:57:49 -0500

    Update scrolling behavior

--- portfolio/src/app/components/Navbar.tsx
+++ portfolio/src/app/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { assets } from "../assets/assets";
 import Image from "next/image";
 
 export default function Navbar() {
   const sideMenuRef = useRef<HTMLUListElement>(null);
+  const [isScroll, setIsScroll] = useState(false);
 
   const openSideMenu = () => {
     if (sideMenuRef.current) {
@@ -17,6 +18,16 @@ export default function Navbar() {
     }
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -26,7 +37,11 @@ export default function Navbar() {
           className="w-full"
         />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+        }`}
+      >
         <a href="#top">
           <Image
             src={assets.logo}
@@ -34,7 +49,11 @@ export default function Navbar() {
             className="w-28 cursor-pointer mr-14"
           />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            isScroll ? "" : "bg-white shadow-sm bg-opacity-50"
+          }`}
+        >
           <li>
             <a className="font-Ovo" href="#top">
               Home
