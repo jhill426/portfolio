c3624f3a551149feb6c6da8ead3ee6e56b3ae539
Justin Hill <jhill@figma.com>
2025-05-02 09:00:17 -0700

    Add half of contact section

---
+++ portfolio/src/app/components/Contact.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div
+      id="contact"
+      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-collor.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
+    >
+      <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
+      <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Welcome to my web development portfolio! Explore a collection of
+        projects showcasing my skills and expertise in web development.
+      </p>
+    </div>
+  );
+}
