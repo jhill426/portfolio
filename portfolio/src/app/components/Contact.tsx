ae0163a481134659874dda44724014ebecdb4501
Justin Hill <jhill@figma.com>
2025-05-05 09:42:25 -0700

    Finish contact information

--- portfolio/src/app/components/Contact.tsx
+++ portfolio/src/app/components/Contact.tsx
@@ -3,6 +3,31 @@ import React from "react";
 import { assets } from "../assets/assets";
 
 export default function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event: any) => {
+    event.preventDefault();
+    setResult("Sending...");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "fabe32a3-2a5b-40f6-9069-6f4e9c476047");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div
       id="contact"
@@ -14,19 +39,21 @@ export default function Contact() {
         Welcome to my web development portfolio! Explore a collection of
         projects showcasing my skills and expertise in web development.
       </p>
-      <form className="max-w-2xl mx-auto">
+      <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
         <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
           <input
             className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
             type="text"
             placeholder="Enter your name"
             required
+            name="name"
           />
           <input
             className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
             type="email"
             placeholder="Enter your email"
             required
+            name="email"
           />
         </div>
         <textarea
@@ -34,6 +61,7 @@ export default function Contact() {
           className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
           placeholder="Enter your message"
           required
+          name="message"
         ></textarea>
         <button
           type="submit"
@@ -42,7 +70,7 @@ export default function Contact() {
           Submit now{" "}
           <Image src={assets.right_arrow_white} alt="" className="m-4" />
         </button>
-        <p className="mt-4">sending...</p>
+        <p className="mt-4">{result}</p>
       </form>
     </div>
   );
